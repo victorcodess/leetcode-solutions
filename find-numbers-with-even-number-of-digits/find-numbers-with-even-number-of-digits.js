9d7364efd2283ce1e5ed41770cc67693b31f9e98
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-14 11:16:33 +0100

    Time: 112 ms, Memory: 44.7 MB - LeetHub

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -6,7 +6,7 @@ var findNumbers = function(nums) {
     let result = 0;
     let numsLength = nums.map(num => Math.floor(Math.log10(num) + 1))
     for(let i = 0; i < numsLength.length; i++){
-        if(numsLength[i] % 2 == 0) {
+        if(numsLength[i] % 2 === 0) {
         result++;
         }
     }
