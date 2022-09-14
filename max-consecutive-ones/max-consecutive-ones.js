774f61a3221585dbdd39bb3df504eba18793fa07
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-14 10:41:29 +0100

    Time: 110 ms, Memory: 44.5 MB - LeetHub

--- max-consecutive-ones/max-consecutive-ones.js
+++ max-consecutive-ones/max-consecutive-ones.js
@@ -4,20 +4,12 @@
  */
 var findMaxConsecutiveOnes = function(nums) {
      let count = 0;
+     let result = 0;
  
-    // initialize max
-    let result = 0;
- 
-    for (let i = 0; i < nums.length; i++) {
-        // Reset count when 0 is found
-        if (nums[i] == 0)
+     for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === 0) {
             count = 0;
- 
-        // If 1 is found, increment
-        // count and update result
-        // if count becomes more.
-        else {
-            // increase count
+        } else {
             count++;
             result = Math.max(result, count);
         }
