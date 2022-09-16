4a8bc5167b184f3e626f8d04c22b5e92e66dd066
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-16 23:10:06 +0100

    Time: 116 ms, Memory: 44.3 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === 0){
+            arr.splice(i, 0, 0)
+            arr.pop()
+          i++
+        }
+      
+    }
+    return arr
+};
