6db02b79019aa8835ade905a6db2e78af44c8ac8
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-19 00:44:48 +0100

    Time: 82 ms (88.26%), Space: 46.3 MB (46.75%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -5,6 +5,10 @@
 var isPalindrome = function (s) {
   let x = s.toLowerCase().replace(/[^A-Za-z0-9]/g, "");
   let y = x.split("").reverse().join("")
-  return x === y ? true : false
+  if (x === y) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
