a54afcd305e99ca1db836e5d50d3f72455b5d528
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-21 18:10:36 +0100

    Time: 84 ms, Memory: 42.2 MB - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -3,5 +3,16 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
-    return arr.filter(e=> arr.includes(e*2))
+  for(let i = 0; i < arr.length; i++) {
+    for(let j = 0; j < arr.length; j++) {
+      if(i !== j) {
+        if(i >= 0 && j < arr.length) {
+          if(arr[i] === 2 * arr[j]) {
+              return true
+          }
+        }
+      }
+    }
+  }
+  return false
 };
