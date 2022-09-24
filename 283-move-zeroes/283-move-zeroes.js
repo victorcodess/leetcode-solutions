fe8b3a4412d76ec796099db8494da9dcb96f0a2a
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-24 09:50:07 +0100

    Time: 118 ms (76.54%), Space: 47.3 MB (10.10%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -3,14 +3,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-   let lastNonZeroFoundAt = 0; 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] != 0) {
-            nums[lastNonZeroFoundAt++] = nums[i];
-        }
+  for(let i = nums.length - 1; i >= 0; i--) {
+    if(nums[i] === 0) {
+      nums.push(nums[i])
+      nums.splice(i, 1)
     }
-    for (let i = lastNonZeroFoundAt; i < nums.length; i++) {
-        nums[i] = 0;
-    }
-    return nums
+  }
 };
