1dfd1a9d2d1dd14bb6ad03c736dd5e2e74227f44
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-27 00:09:38 +0100

    Time: 99 ms (55.90%), Space: 44.8 MB (25.20%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.js
+++ 414-third-maximum-number/414-third-maximum-number.js
@@ -2,26 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-let thirdMax = function(nums) {
-    // Sort the array in non-increasing order.
-    nums.sort((a, b) => b - a);
-
-    let elemCounted = 1;
-    let prevElem = nums[0];
-
-    for (let index = 1; index < nums.length; ++index) {
-        // Current element is different from previous.
-        if (nums[index] != prevElem) {
-            elemCounted += 1;
-            prevElem = nums[index];
-        }
-
-        // If we have counted 3 numbers then return current number.
-        if (elemCounted == 3) {
-            return nums[index];
-        }
-    }
-
-    // We never counted 3 distinct numbers, return largest number.
-    return nums[0];
+var thirdMax = function(nums) {
+  let disMax;
+  let sortedArr = [...new Set(nums)].sort((a, b) => a - b)
+  let last = sortedArr.length - 1
+  if(sortedArr.length < 3) {
+   disMax = sortedArr[last]
+  } else {
+    let thirdDis = sortedArr[sortedArr.length - 3]
+    disMax = thirdDis
+  }
+   return disMax 
 };
