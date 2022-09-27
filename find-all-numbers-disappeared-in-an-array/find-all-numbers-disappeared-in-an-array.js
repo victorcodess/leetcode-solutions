91c164f0dc41aea3e8c64022aa4c3a4a5ef101b7
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-27 01:29:14 +0100

    Time: 178 ms, Memory: 53.2 MB - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -3,17 +3,13 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-  let result = [];
-  let expectedArr = []
-  nums.sort((a, b) => a - b)
-  for(let i = 1; i <= nums.length; i++) {
-    expectedArr.push(i)
-  }
-  let disArr = [...new Set(nums)]
-  for(let j = 0; j < expectedArr.length; j++) {
-    if(!disArr.includes(expectedArr[j])) {
-      result.push(expectedArr[j])
+  const disArr = new Set(nums);    
+  const result = [];
+    
+  for (let i = 1; i <= nums.length; i++) {
+	if(!disArr.has(i)) {
+		result.push(i);
+	    }
     }
-  }
-    return result
+    return result;
 };
