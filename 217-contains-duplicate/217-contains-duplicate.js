b39f955f0dffb7a11ce54d62edc5a78d56679a1d
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-09-27 22:00:08 +0100

    Time: 123 ms (70.97%), Space: 50.5 MB (73.08%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,11 +3,13 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-  nums.sort((a, b) => a - b)
-  for(let i = 0; i < nums.length; i++) {
-    if(nums[i] === nums[i + 1]) {
-      return true
-    }
-  }
-  return false
+  // nums.sort((a, b) => a - b)
+  // for(let i = 0; i < nums.length; i++) {
+  //   if(nums[i] === nums[i + 1]) {
+  //     return true
+  //   }
+  // }
+  // return false
+    
+    return nums.length !== new Set(nums).size
 };
