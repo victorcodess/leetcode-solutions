7e289b45f2c26503046bfe0a568ee301a8bf83af
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-10-02 17:08:59 +0100

    Time: 183 ms (13.41%), Space: 47.6 MB (31.95%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,8 +4,5 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    let ss = s.split("").sort()
-    let tt = t.split("").sort()
-    
-  return ss.join("") === tt.join("")
+  return s.split("").sort().join("") === t.split("").sort().join("")
 };
