f40b291f1797536a1c7d6d73296965db2b4ff4ae
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-10-13 15:56:52 +0100

    Time: 132 ms (5.12%), Space: 41.6 MB (89.51%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -5,11 +5,9 @@
  */
 var strStr = function(haystack, needle) {
     for(let i = 0; i < haystack.length; i++) {
-        if(haystack[i] == needle[0]) {
-           if(haystack.substring(i, i + needle.length) == needle) {
-               return i
-           }
-        } 
+        if(haystack.substring(i, i + needle.length) == needle) {
+            return i
+        }
     }
     return -1
 };
