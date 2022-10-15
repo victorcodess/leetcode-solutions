7f0109224c4d71a6456ebef2b1290fcb48503bf1
Victor Williams <84178696+victorcodess@users.noreply.github.com>
2022-10-15 08:55:09 +0100

    Time: 85 ms, Memory: 42.1 MB - LeetHub

---
+++ longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+  strs.sort();
+  for (let i = 0; i < strs[0].length; i++) {
+    if (strs[0][i] !== strs[strs.length - 1][i]) {
+         return strs[0].substr(0, i);
+    }
+  }
+  return strs[0];
+};
